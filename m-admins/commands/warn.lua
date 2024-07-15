a549f3a07e7c806d55fda77dda457c0c9435e05c
borsuczyna <libxxerty@gmail.com>
2024-07-15 16:41:31 +0200

    warns

---
+++ m-admins/commands/warn.lua
@@ -0,0 +1,21 @@
+addCommandHandler('warn', function(player, cmd, playerToFind, ...)
+    if not getElementData(player, 'player:uid') then return end
+    if not doesPlayerHavePermission(player, 'command:warn') then
+        exports['m-notis']:addNotification(player, 'error', 'Błąd', 'Nie posiadasz uprawnień')
+        return
+    end
+
+    local reason = table.concat({...}, ' ')
+    local foundPlayer = exports['m-core']:getPlayerFromPartialName(playerToFind)
+    if not foundPlayer then
+        exports['m-notis']:addNotification(player, 'error', 'Błąd', 'Nie znaleziono gracza')
+        return
+    end
+
+    local playerName = getPlayerName(player)
+    local foundPlayerName = getPlayerName(foundPlayer)
+
+    triggerClientEvent(foundPlayer, 'onClientShowWarn', resourceRoot, getPlayerName(player), reason)
+    exports['m-logs']:sendLog('admin', 'warning', ('Admin `%s` ostrzegł gracza `%s`: `%s`'):format(playerName, foundPlayerName, reason))
+    exports['m-notis']:addNotification(player, 'success', 'Warn', ('Ostrzegłeś gracza %s'):format(foundPlayerName))
+end)
