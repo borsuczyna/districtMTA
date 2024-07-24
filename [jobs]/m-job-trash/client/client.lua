9ada42418206bfe7886571d6004cffdef43fbbc8
borsuczyna <libxxerty@gmail.com>
2024-07-24 23:56:40 +0200

    trash job 1/2

--- [jobs]/m-job-trash/client/client.lua
+++ [jobs]/m-job-trash/client/client.lua
@@ -1,3 +1,7 @@
+local renderTarget = false
+local lastRenderTargetUpdate = 0
+local fonts = {}
+
 local marker = createMarker(settings.jobStart + Vector3(0, 0, -1), 'cylinder', 1, 255, 140, 0, 0)
 setElementData(marker, 'marker:icon', 'work')
 setElementData(marker, 'marker:title', 'Wywóz śmieci')
@@ -13,4 +17,41 @@ addEventHandler('onClientMarkerLeave', marker, function(player, md)
     if player ~= localPlayer or not md then return end
     
     exports['m-jobs']:hideJobGui()
+end)
+
+function updateRenderTarget()
+    if not fonts or not fonts[1] then
+        fonts = {
+            exports['m-ui']:getFont('Inter-Bold', 25),
+            exports['m-ui']:getFont('Inter-Medium', 20)
+        }
+    end
+
+    if not renderTarget then
+        renderTarget = dxCreateRenderTarget(430, 100, true)
+    end
+
+    local dumpLevel = exports['m-jobs']:getJobData('trashLevel') or 0
+
+    dxSetRenderTarget(renderTarget, true)
+    dxDrawText('Wywóz śmieci', 0, 0, 430, 50, tocolor(255, 255, 255), 1, fonts[1], 'center', 'bottom')
+    dxDrawText(('Zapełnienie: %.2fkg'):format(dumpLevel), 0, 50, 430, 50, tocolor(255, 255, 255), 1, fonts[2], 'center', 'top')
+    dxSetRenderTarget()
+end
+
+addEventHandler('onClientPreRender', root, function()
+    if getElementData(localPlayer, 'player:job') ~= 'trash' then return end
+
+    local jobVehicle = getElementData(localPlayer, 'player:jobVehicle')
+    if not jobVehicle or not isElement(jobVehicle) then return end
+
+    local x, y, z = getPositionFromElementOffset(jobVehicle, 0, -4, 0)
+    local lx, ly, lz = getPositionFromElementOffset(jobVehicle, 0, -5, 0)
+
+    if not renderTarget or getTickCount() - lastRenderTargetUpdate > 1000 then
+        updateRenderTarget()
+        lastRenderTargetUpdate = getTickCount()
+    end
+
+    dxDrawMaterialLine3D(x, y, z + 0.25, x, y, z - 0.25, renderTarget, 0.5 * (430 / 100), tocolor(255, 255, 255), lx, ly, lz)
 end)
--- [jobs]/m-job-trash/server/server.lua
+++ [jobs]/m-job-trash/server/server.lua
@@ -1,8 +1,18 @@
 addEvent('jobs:finishJob', true)
 addEvent('jobs:startJob', true)
 addEvent('jobs:finishJobLobby', true)
+addEvent('jobs:trash:trashHit')
 
 local vehicles = {}
+local timers = {}
+
+function setPlayerTimer(player, callback, time, ...)
+    if timers[player] and isTimer(timers[player]) then
+        killTimer(timers[player])
+    end
+
+    timers[player] = setTimer(callback, time, 1, ...)
+end
 
 addEventHandler('jobs:startJob', root, function(job, hash, players)
     if job ~= 'trash' then return end
@@ -23,17 +33,46 @@ addEventHandler('jobs:startJob', root, function(job, hash, players)
     setVehicleEngineState(vehicle, true)
     setElementFrozen(vehicle, true)
     setVehicleOverrideLights(vehicle, 1)
-
     vehicles[hash] = vehicle
 
+    local marker = exports['m-jobs']:createLobbyMarker(hash, 'cylinder', 0, 0, 0, 1, 50, 140, 255, 0, {
+        icon = 'work',
+        title = 'Wywóz śmieci',
+        desc = 'Tył śmieciarki',
+        attach = {
+            element = vehicle,
+            position = {0, -4.5, -1.5},
+        }
+    })
+
     for k,v in pairs(trash) do
-        exports['m-jobs']:createLobbyObject(hash, 1337, v[1], v[2], v[3] - 0.4, 0, 0, v[6] + 180)
-        exports['m-jobs']:createLobbyBlip(hash, v[1], v[2], v[3], 41, 500)
+        local object = exports['m-jobs']:createLobbyObject(hash, 1337, v[1], v[2], v[3] - 0.4, 0, 0, v[6] + 180, {
+            noTrigger = true,
+            colshape = {
+                size = 1,
+                event = 'jobs:trash:trashHit'
+            },
+            blip = {
+                icon = 41,
+                visibleDistance = 500
+            }
+        })
+    end
+
+    exports['m-jobs']:updateLobbyObjects(hash)
+    exports['m-jobs']:setLobbyData(hash, 'trashLevel', 0)
+
+    for k,v in pairs(players) do
+        setElementData(v, 'player:jobVehicle', vehicle)
     end
 end)
 
 addEventHandler('jobs:finishJob', root, function(job, hash, player)
     if job ~= 'trash' then return end
+
+    removeElementData(player, 'player:animation')
+    removeElementData(player, 'player:carryBin')
+    toggleAllControls(player, true)
 
     local vehicle = vehicles[hash]
     if vehicle and isElement(vehicle) then
@@ -56,4 +95,32 @@ addEventHandler('onResourceStop', resourceRoot, function()
             exports['m-jobs']:leaveJob(v)
         end
     end
+end)
+
+local function pickUpTrash(player, hash, objectHash)
+    setElementFrozen(player, false)
+    setPedAnimation(player)
+    setElementData(player, 'player:animation', 'carry')
+    setElementData(player, 'player:carryBin', objectHash)
+    toggleAllControls(player, true)
+    toggleControl(player, 'crouch', false)
+    toggleControl(player, 'jump', false)
+    toggleControl(player, 'sprint', true) -- TODO add sprint upgrade
+
+    exports['m-jobs']:attachObjectToPlayer(hash, objectHash, player, 23, 0.44, 0, 0.45, 0, 220, 90)
+end
+
+addEventHandler('jobs:trash:trashHit', root, function(hash, player, objectHash)
+    if getElementData(player, 'player:carryBin') then return end
+
+    exports['m-jobs']:setLobbyObjectOption(hash, objectHash, 'colshape', false)
+    exports['m-jobs']:setLobbyObjectOption(hash, objectHash, 'blip', false)
+    
+    setElementFrozen(player, true)
+    toggleAllControls(player, false)
+    setPedAnimation(player, "CARRY", "putdwn", -1, false)
+
+    setPlayerTimer(player, pickUpTrash, 1000, player, hash, objectHash)
+
+    -- exports['m-jobs']:attachObjectToPlayer(hash, objectHash, player, 'head', 0.1, -0.05, 0, 0, 90, 0)
 end)
