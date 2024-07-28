589b8eca5bcc5f93799dd7e4bde3a2fa0af7d7ab
fuckbtc <niezlygejniezlygej@gmail.com>
2024-07-28 23:26:31 +0200

    Update trigger-locker.lua
    remove some useless codes

--- m-anticheat/server/trigger-locker.lua
+++ m-anticheat/server/trigger-locker.lua
@@ -1,6 +1,6 @@
 function setPlayerTriggerLocked(player, state, reason)
     setElementData(player, 'player:triggerLocked', state)
-    -- exports['m-logs']:sendLog('anticheat', 'error', ('Locked player %s for all triggers: %s'):format(getPlayerName(player), reason or 'No reason provided'))
+
     if state then
         exports['m-log']:addLog('anticheat', 'error', ('Locked player %s for all triggers: %s'):format(getPlayerName(player), reason or 'No reason provided'))
     else
@@ -11,7 +11,3 @@ end
 function isPlayerTriggerLocked(player)
     return getElementData(player, 'player:triggerLocked')
 end
-
-addCommandHandler('test11', function()
-    createExplosion(713.52167, -522.02460, 16.32814,1)
-end)
