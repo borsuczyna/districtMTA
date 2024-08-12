5c6e67b4a5ef0078c534279f02d6c74faaee85b8
fuckbtc <niezlygejniezlygej@gmail.com>
2024-08-12 17:41:35 +0200

    atms styling

--- m-atm/shared.lua
+++ m-atm/shared.lua
@@ -1,5 +1,5 @@
 local atms = {
-    {1105.074, -1827.607, 33.629, -0.000, 0.000, 353.770},
+    {224.142, 142.2, 1003.023, 0, 0, 90, 3},
 }
 
 local function createAtm(atm)
@@ -9,6 +9,9 @@ local function createAtm(atm)
         local atmObject = createObject(2942, x, y, z - 0.35, rx, ry, rz + 180)
         setElementFrozen(atmObject, true)
         setObjectBreakable(atmObject, false)
+        if atm[7] then
+            setElementInterior(atmObject, atm[7])
+        end
 
         local colShape = createColSphere(x, y, z, 1.5)
         attachElements(colShape, atmObject, 0, 0, 0)
@@ -16,6 +19,10 @@ local function createAtm(atm)
         addEventHandler('onClientColShapeLeave', colShape, onAtmLeave)
     else
         local blip = createBlip(x, y, z, 53, 2, 255, 0, 0, 255, 0, 9999)
+        setElementData(blip, 'blip:hoverText', 'Bankomat')
+        if atm[7] then
+            setElementInterior(blip, atm[7])
+        end
     end
 end
 
